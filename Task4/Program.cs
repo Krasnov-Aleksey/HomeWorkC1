ae851ea742a61a82f1ac310038f1b919c3a33e32
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-09 15:53:48 +0300

    Задание 2

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max<number2)
+    max=number2;
+if (max<number3)
+    max=number3;
+
+Console.Write($"max={max}");
