306cddf6924198e7ed8fd41dcac461d622e3cec3
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-09 15:28:29 +0300

    Задание1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 2 Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-ое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+    Console.Write($"max={number1}");
+else
+    Console.Write($"max={number2}");
+
