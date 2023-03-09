2a53c867b6941bfa0a368ea03d6602a15886745d
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-09 16:39:49 +0300

    Задание 4

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+for (int i=1;i<=number1;i++)
+{
+    if (i % 2 == 0)
+        Console.Write($" {i}");
+}
+
+
