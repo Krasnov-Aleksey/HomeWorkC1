5d6d8755e5084ce70b7bdfcc218ba3e0b487a8d3
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-09 16:23:09 +0300

    Задание 3

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+double number2;
+number2=number1%2;
+if (number2 == 0)
+    Console.Write("Число четное");
+else
+    Console.Write("Число нечетное");
